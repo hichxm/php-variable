5f20466121baaf1c43d2d32f6bf6fa98eabe0a23
Ilya Bakhlin <ilya.bakhlin@quorrax.com>
2021-06-28 09:08:46 +0200

    Implementing the Variable interface.

---
+++ src/Classes/Variable.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Quorrax\Classes;
+
+use Quorrax\Interfaces\Variable as VariableInterface;
+
+/**
+ * @package Quorrax\Classes
+ */
+class Variable implements VariableInterface
+{
+}
